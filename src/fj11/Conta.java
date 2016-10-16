e970e00c686d4375cf5b7f9d54c40d8332abeeb8
Elis Fernanda <fernandalisses@gmail.com>
2016-10-16 21:49:44 -0200

    Revisando OB java apostila Caelum

---
+++ src/fj11/Conta.java
@@ -0,0 +1,9 @@
+package fj11;
+
+public class Conta {
+	int numero;
+	String dono;
+	double saldo;
+	double limite;
+
+}
---
+++ src/fj11/Programa.java
@@ -0,0 +1,11 @@
+package fj11;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		//o new - está instânciando a conta. Criando em seguida objeto conta
+		Conta minhaConta = new Conta();
+
+	}
+
+}
